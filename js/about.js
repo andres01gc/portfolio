2b5dbc33c771fd3669c2ac82feea2b8aac725171
gc <anfgc01@gmail.com>
2018-05-09 04:56:29 -0500

    detallles

--- js/about.js
+++ js/about.js
@@ -6,14 +6,24 @@ var maskIzq = $(".imizq");
 $(".main-view").mousemove(function (event) {
     var msg = "Handler for .mousemove() called at ";
     // msg += event.pageX + ", " + event.pageY;
-
     var angle = 7;
 
-    var upValue = 60;
-    var downValue = 40;
+    // parte izq
+    // var upValue1_y = 0;
+    // var downValue1_y = 0;
 
-    document.getElementById('mizq').style.clipPath = " polygon(0 0," + upValue + "% 0," + downValue + "% 100%, 0 100%)";
-    document.getElementById('mider').style.clipPath = "polygon(" + upValue + "% 0, 100% 0, 100% 100%,"+downValue+"% 100%)";
+    // var upValue2_y = 50;
+    // var downValue2_y = 50;
+
+    // parte der
+    var upValue1_y = 50;
+    var downValue1_y = 50;
+
+    var upValue2_y = 0;
+    var downValue2_y = 0;
+
+    // document.getElementById('mizq').style.clipPath = " polygon(0 0," + upValue1_y+1 + "% 0," + downValue1_y+1 + "% 100%, 0 100%)";
+    document.getElementById('mider').style.clipPath = "polygon(" + upValue1_y + "% 0, " + upValue2_y + "% 0, " + downValue2_y + "% 100%," + downValue1_y + "% 100%)";
 
     // maskIzq.css("");
     // maskIzq.append( "<div>" + msg + "</div>" );
